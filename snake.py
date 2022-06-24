ac8703ac4dcd01726b07c6bb545b911588e7ab3c
Chmielek29 <98705769+Chmielek29@users.noreply.github.com>
2022-06-24 15:38:16 +0200

    Update snake.py

--- snake.py
+++ snake.py
@@ -12,7 +12,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
-        #self.head = self.segments[0]
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -29,10 +29,10 @@ class Snake:
         self.add_segment(self.segments[-1].position())
 
     def move(self):
-        #for seg_num in range(len(self.segments) - 1, 0, -1):
-            #new_x = self.segments[seg_num - 1].xcor()
-            #new_y = self.segments[seg_num - 1].ycor()
-            #self.segments[seg_num].goto(new_x, new_y)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
 
     def up(self):
